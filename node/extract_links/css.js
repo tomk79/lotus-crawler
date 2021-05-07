a8da6610267b4147195667063b924054e8f1bf95
Tomoya Koyanagi <tomk79@gmail.com>
2021-05-07 22:07:00 +0900

    CSS中のリンクを追跡できるようになった。

--- node/extract_links/css.js
+++ node/extract_links/css.js
@@ -33,10 +33,99 @@ module.exports = function( main, dba ){
 	 * CSSコード中のリンクを抽出する
 	 */
 	this.parseCssFile = function(baseUrl, srcCss, callback){
-		console.log('----- parseCssFile:', baseUrl, srcCss);
-		// TODO: CSSコード中のURL抽出が未実装
+		// console.log('----- parseCssFile:', baseUrl, srcCss);
 		let rtn = [];
+
+		function preg_quote(str){
+			str = str.split('"').join('\\"');
+			return str;
+		}
+		function trim(str){
+			str = str.replace(/^\s*/, '');
+			str = str.replace(/\s*$/, '');
+			return str;
+		}
+
+
+		let targetUri;
+		let srcAfter = '';
+
+		// url()
+		while( 1 ){
+			if( !srcCss.match( /^([\s\S]*?)(\/\*|url\s*\(\s*(\"|\'|))([\s\S]*)$/si ) ){
+				srcAfter += srcCss;
+				break;
+			}
+			srcAfter += RegExp.$1;
+			let $start = RegExp.$2;
+			let $delimiter = RegExp.$3;
+			srcCss = RegExp.$4;
+
+			if( $start == '/*' ){
+				srcAfter += '/*';
+
+				srcCss.match( /^([\s\S]*?)\*\/([\s\S]*)$/si );
+				srcAfter += RegExp.$1;
+				srcAfter += '*/';
+				srcCss = RegExp.$2;
+			}else{
+				srcAfter += 'url("';
+				if( srcCss.match( new RegExp('^([\\s\\S]*?)'+preg_quote($delimiter, '/')+'\\s*\\)([\\s\\S]*)', 'si') ) ){
+					targetUri = RegExp.$1;
+					srcCss = RegExp.$2;
+					rtn.push({
+						url: main.resolveLinkToUri( baseUrl, trim( targetUri ) ),
+						method: 'GET',
+					});
+					srcAfter += targetUri;
+				}
+
+				srcAfter += '")';
+			}
+
+		}
+
+		// @import
+		srcCss = srcAfter;
+		srcAfter = '';
+		while( 1 ){
+			if( !srcCss.match( /^([\s\S]*?)@import\s*([^\s\;]*)([\s\S]*)$/si ) ){
+				srcAfter += srcCss;
+				break;
+			}
+			srcAfter += RegExp.$1;
+			srcAfter += '@import ';
+			targetUri = RegExp.$2;
+			srcCss = RegExp.$3;
+
+			targetUri = trim( targetUri );
+
+			if( !targetUri.match(/^url\s*\(/) ){
+				srcAfter += '"';
+				if( targetUri.match( /^(\"|\')(.*)\1$/si ) ){
+					targetUri = RegExp.$2;
+					targetUri = trim( targetUri );
+				}
+
+				rtn.push({
+					url: main.resolveLinkToUri( baseUrl, targetUri ),
+					method: 'GET',
+				});
+
+				srcAfter += targetUri;
+				srcAfter += '"';
+			}else{
+				srcAfter += targetUri;
+			}
+		}
+
+		// console.log(srcAfter);
+
+
+
+		// console.log(rtn);
 		callback( rtn );
+		return;
 	}
 
 }
